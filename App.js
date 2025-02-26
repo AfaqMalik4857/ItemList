b0ee27f794eeeffa9b261362aa48f17fc715736d
Afaq Ahmad <L1F21BSSE0622@UCP.EDU.PK>
2025-02-26 23:08:02 +0500

    Update the  file

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./Redux/Store";
 import ListScreen from "./Screen/ListScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ListScreen />
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={styles.container}>
+          <ListScreen />
+          <StatusBar style="auto" />
+        </View>
+      </PersistGate>
+    </Provider>
   );
 }
 
